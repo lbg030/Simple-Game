25cd441faff984e9cad57569a7b5f907c02d1d9a
ByeongGwon <lee971009@naver.com>
2022-08-02 01:45:55 +0900

    큰 공 추가

---
+++ Pygame/pygame_project/3_ball_movement.py
@@ -0,0 +1,135 @@
+import pygame
+import os
+#----------------------------------------------------------------#
+
+# 기본 초기화 ( 반드시 해야 할 것들 )
+pygame.init() # 반드시 해줘야 되는 부분
+
+# 화면 크기 
+screen_width = 640 # 가로
+screen_height = 480 # 세로
+
+screen = pygame.display.set_mode((screen_width, screen_height)) # 480 * 640
+
+#화면 타이틀 설정
+pygame.display.set_caption("Nado Pang") #게임 이름
+
+#FPS
+clock = pygame.time.Clock()
+
+#--------------------------------------------------------#
+
+# 1. 사용자 게임 초기화( 배경화면, 게임 이미지, 좌표, 속도, 폰트 등 )
+current_path = os.path.dirname(os.path.realpath(__file__)) #현재 파일 위치 반환
+image_path = os.path.join(current_path, "images") # 이미지 폴더 위치 반환
+
+#배경 만들기
+background = pygame.image.load(os.path.join(image_path, "background.png"))
+
+#스테이지 만들기
+stage = pygame.image.load(os.path.join(image_path, "stage.png"))
+stage_size = stage.get_rect().size
+stage_height = stage_size[1] #스테이지의 높이 위에 캐릭터를 두기 위해 사용
+
+#캐릭터 만들기
+character = pygame.image.load(os.path.join(image_path, "character.png"))
+character_size = character.get_rect().size
+character_width = character_size[0]
+character_height = character_size[1]
+character_x_pos = (screen_width / 2) - (character_width / 2)
+character_y_pos = (screen_height - character_height - stage_height)
+
+#캐릭터 이동 방향
+character_to_x = 0
+
+# 캐릭터 이동 속도
+character_speed = 5
+
+# 무기 만들기
+weapon = pygame.image.load(os.path.join(image_path, "weapon.png"))
+weapon_size = weapon.get_rect().size
+weapon_width = weapon_size[0]
+
+# 무기는 한번에 여러 발 발사 가능
+weapons = []
+
+#무기 이동 속도
+weapon_speed = 10
+
+# 공 만들기 ( 4개 크기에 대해 따로 처리)
+ball_images = [
+    pygame.image.load(os.path.join(image_path, "balloon1.png")),
+    pygame.image.load(os.path.join(image_path, "balloon2.png")),
+    pygame.image.load(os.path.join(image_path, "balloon3.png")),
+    pygame.image.load(os.path.join(image_path, "balloon4.png"))]
+
+# 공 크기에 따른 최초 스피드
+ball_speed_y = [-18,-15,-12,-9] # index 0,1,2,3 에 해당하는 값
+
+# 공들
+balls = []
+
+#최초 발생하는 큰 공 추가
+balls.append({
+    "pos_x" : 50,
+    "pos_y" : 50,
+    "img_idx" : 0,
+    "to_x": 3, # x축 이동방향, -3이면 왼쪽으로, 3이면 오른쪽으로
+    "to_y": -6, # y축 이동방향
+    "init_spd_y": ball_speed_y[0] # y 최초속도
+})
+
+running = True # 게임이 진행중인지 아닌지 확인하는거 = flag
+while running :
+    dt = clock.tick(30) #게임 화면의 초당 프레임 수를 설정
+    # print("FPS : " + str(clock.get_fps())) # 프레임 확인
+    
+    # 2. 이벤트 처리 ( 키보드, 마우스 등 )
+    for event in pygame.event.get(): # 어떤 이벤트가 발생하는지
+        if event.type == pygame.QUIT: # 창이 닫히는 이벤트가 발생하면 ( 안쓰면 꺼지지 않음 )
+            running = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                character_to_x -= character_speed
+            elif event.key == pygame.K_RIGHT:
+                character_to_x += character_speed
+            elif event.key == pygame.K_SPACE: # 무기 발사
+                weapon_x_pos = character_x_pos + ( character_width/2) - (weapon_width/2)
+                weapon_y_pos = character_y_pos
+                weapons.append([weapon_x_pos, weapon_y_pos])
+                
+            
+        if event.type == pygame.KEYUP: #방향키를 떼면 멈춤
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                character_to_x = 0
+    # 3. 게임 캐릭터 위치 정의
+    character_x_pos += character_to_x
+    
+    if character_x_pos < 0 :
+        character_x_pos = 0
+    elif character_x_pos > screen_width - character_width :
+        character_x_pos = screen_width- character_width
+    
+    # 무기 위치 조정
+    weapons = [[w[0], w[1] - weapon_speed] for w in weapons] # 무기 위치를 위로
+    
+    #천장에 닿은 무기 없애기
+    weapons = [ [w[0], w[1]] for w in weapons if w[1] > 0 ]
+    # 4. 충돌 처리
+    
+    # 5. 화면에 그리기
+    screen.blit(background, (0,0))
+    
+    for weapon_x_pos, weapon_y_pos in weapons:
+        screen.blit(weapon, (weapon_x_pos, weapon_y_pos))
+        
+    screen.blit(stage, (0, screen_height - stage_height))
+    screen.blit(character, (character_x_pos, character_y_pos))
+    
+    
+    
+    pygame.display.update() #게임 화면을 다시 그리기 ! (반드시 계속 호출 되어야 되는 부분)
+    
+# 파이게임 종료
+pygame.quit()
