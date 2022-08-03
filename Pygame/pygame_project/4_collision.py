efa62d1178f4e7ff821d80c2c8da03491a8ebefc
ByeongGwon <lee971009@naver.com>
2022-08-03 20:09:17 +0900

    맞으면 공 지우기 ( 2시간 19분 52초)

--- Pygame/pygame_project/4_collision.py
+++ Pygame/pygame_project/4_collision.py
@@ -79,6 +79,10 @@ balls.append({
     "init_spd_y": ball_speed_y[0] # y 최초속도
 })
 
+# 사라질 무기, 공 정보 저장 변수
+weapon_to_remove = -1
+ball_to_remove = -1
+
 running = True # 게임이 진행중인지 아닌지 확인하는거 = flag
 while running :
     dt = clock.tick(30) #게임 화면의 초당 프레임 수를 설정
@@ -145,12 +149,59 @@ while running :
         
     # 4. 충돌 처리
     
+    # 캐릭터 rect 정보 업데이트
+    character_rect = character.get_rect()
+    character_rect.left = character_x_pos
+    character_rect.top = character_y_pos
+    print(character_rect)
     
+    for ball_idx, ball_val in enumerate(balls):
+        ball_pos_x = ball_val["pos_x"]
+        ball_pos_y = ball_val["pos_y"]
+        ball_img_idx = ball_val["img_idx"]    
+        
+        #공 rect 정보 업데이트
+        ball_rect = ball_images[ball_img_idx].get_rect()
+        ball_rect.left = ball_pos_x
+        ball_rect.top = ball_pos_y
+        
+        #공과 캐릭터 충돌 처리
+        if character_rect.colliderect(ball_rect):
+            running = False
+            break
+        
+        #공과 무기들 충돌 처리
+        for weapon_idx, weapon_val in enumerate(weapons):
+            weapon_pos_x = weapon_val[0]
+            weapon_pos_y = weapon_val[1]
+        
+            #무기 rect 정보 업데이트
+            weapon_rect = weapon.get_rect()
+            weapon_rect.left = weapon_pos_x
+            weapon_rect.top = weapon_pos_y
+            
+            #충돌 체크
+            if weapon_rect.colliderect(ball_rect):
+                weapon_to_remove = weapon_idx # 해당 무기 없애기 위한 값 설정
+                ball_to_remove = ball_idx # 해당 공 없애기 위한 값 설정
+                break
+        
+    # 충돌 된 공 or 무기 없애기
+    if ball_to_remove > -1 :
+        del balls[ball_to_remove]
+        ball_to_remove = -1
+        
+    if weapon_to_remove > -1 :
+        del weapons[weapon_to_remove]
+        weapon_to_remove = -1
+        
+        
     # 5. 화면에 그리기
     screen.blit(background, (0,0))
     
     for weapon_x_pos, weapon_y_pos in weapons:
         screen.blit(weapon, (weapon_x_pos, weapon_y_pos))
+        
     for idx, val in enumerate(balls):
         ball_pos_x = val["pos_x"]
         ball_pos_y = val["pos_y"]
