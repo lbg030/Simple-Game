7f0f00bc1ad28b91517006a9286bb9c0a696497f
ByeongGwon <lee971009@naver.com>
2022-08-07 14:09:22 +0900

    main

--- Pygame/Zelda/main.py
+++ Pygame/Zelda/main.py
@@ -1,25 +1,27 @@
 import pygame, sys
-import settings
+from settings import *
+from debug import debug
 
 class Game:
-	def __init__(self):
-		  
-		# general setup
-		pygame.init()
-		self.screen = pygame.display.set_mode((settings.WIDTH,settings.HEIGTH))
-		self.clock = pygame.time.Clock()
-	
-	def run(self):
-		while True:
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					pygame.quit()
-					sys.exit()
-
-			self.screen.fill('black')
-			pygame.display.update()
-			self.clock.tick(settings.FPS)
+    def __init__(self):
+          
+        # general setup
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
+        self.clock = pygame.time.Clock()
+    
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+     
+            self.screen.fill('black');
+            debug('hello :)')
+            pygame.display.update()
+            self.clock.tick(FPS)
 
 if __name__ == '__main__':
-	game = Game()
-	game.run()
+    game = Game()
+    game.run()
--- Pygame/pygame_basic/8_frame.py
+++ Pygame/pygame_basic/8_frame.py
@@ -5,8 +5,8 @@ import pygame
 pygame.init() # 반드시 해줘야 되는 부분
 
 # 화면 크기 
-screen_width = 480 # 가로
-screen_height = 640 # 세로
+screen_width = 1920 # 가로
+screen_height = 1080 # 세로
 screen = pygame.display.set_mode((screen_width, screen_height)) # 480 * 640
 
 #화면 타이틀 설정
