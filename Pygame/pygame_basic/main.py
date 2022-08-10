9a7632dc4a7a3cecc35a14742f2831b868e7d4d2
ByeongGwon <lee971009@naver.com>
2022-08-10 22:00:55 +0900

    점프기능 미완성

--- Pygame/pygame_basic/main.py
+++ Pygame/pygame_basic/main.py
@@ -1,181 +1,218 @@
-import pygame, sys
+import sys
+import pygame
 
-                
-pygame.init()
-clock = pygame.time.Clock()
+ 
 
-screen_width = 1280 # 가로
-screen_height = 720 # 세로
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("testing game")
+# 게임 화면 크기
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 500
 
+ 
 
+# 색상
+WHITE = (255,255,255)
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self, pos_x, pos_y):
-        super().__init__()
-        
-        # Move sprites
-        self.sprites = []
-        self.Rsprites = []
-        
-        self.direction = True
-        self.is_animating = False
-        self.is_attack = False
-        
-        self.sprites.append(pygame.image.load('image/메인남캐_Move_01.gif'))
-        self.sprites.append(pygame.image.load('image/메인남캐_Move_02.gif'))
-        self.sprites.append(pygame.image.load('image/메인남캐_Move_03.gif'))
-        self.sprites.append(pygame.image.load('image/메인남캐_Move_04.gif'))
-        self.sprites.append(pygame.image.load('image/메인남캐_Move_05.gif'))
-        self.sprites.append(pygame.image.load('image/메인남캐_Move_06.gif'))
-        
-        self.current_sprite = 0
-        self.current_attack_sprite = 0
-        
-        self.image = self.sprites[self.current_sprite]
-        
+ 
+
+# 속도와 질량 기본 값
+VELOCITY = 7
+MASS = 2
+ 
+
+class Car:
+
+ 
+
+    def __init__(self):
+        self.image = ""
+        self.dx = 0
+        self.dy = 0
+        self.rect = ""
+        self.isJump = 0
+        self.v = VELOCITY # 속도
+        self.m = MASS  # 질량
+
+ 
+
+    def load_car(self):
+        # 플레이어 차량
+        self.image = pygame.image.load("smallcar.png")
+        # 크기 조정
+        self.image = pygame.transform.scale(self.image, (150, 57))
         self.rect = self.image.get_rect()
-        self.rect.y = pos_y
-        # self.rect.topleft = [pos_x, pos_y]
-        
-        #----------------------------------------------------------------
-        ## 이미지 스케일링 ( 사이즈 조절 )
-        # for idx,x in enumerate(self.sprites):
-        #     x = pygame.transform.scale(x, (200,100))
-        #     self.sprites[idx] = x
-        
-        # 오른쪽 이미지
-        for i in (self.sprites):
-            self.Rsprites.append(pygame.transform.flip(i, True, False))    
+        self.rect.centerx = round(WINDOW_WIDTH / 2)
+        self.rect.bottom = WINDOW_HEIGHT
 
-        # 속도
-        self.velocity_x = 0
+ 
 
-        #--------------------------------------------------------
-        
-        # Attack sprites
-        self.attack_sprites = []
-        self.attack_Rsprites = []
-        
-        self.attack_sprites.append(pygame.image.load('image/메인남캐_Attack_01.gif'))
-        self.attack_sprites.append(pygame.image.load('image/메인남캐_Attack_02.gif'))
-        self.attack_sprites.append(pygame.image.load('image/메인남캐_Attack_03.gif'))
-        self.attack_sprites.append(pygame.image.load('image/메인남캐_Attack_04.gif'))
-        self.attack_sprites.append(pygame.image.load('image/메인남캐_Attack_05.gif'))
-        self.attack_sprites.append(pygame.image.load('image/메인남캐_Attack_06.gif'))
-        self.attack_sprites.append(pygame.image.load('image/메인남캐_Attack_07.gif'))
-        self.attack_sprites.append(pygame.image.load('image/메인남캐_Attack_08.gif'))
-        
-        for i in (self.attack_sprites):
-            self.attack_Rsprites.append(pygame.transform.flip(i, True, False))  
-        
-    def animate(self):
-        self.is_animating = True
-        self.velocity_x = 2
-        
-    def unanimate(self):
-        self.is_animating = False
-        self.is_attack = False
-        self.velocity_x = 0
-        
+    # 자동차를 스크린에 그리기
+    def draw_car(self):
+        SCREEN.blit(self.image, [self.rect.x, self.rect.y])
+
+ 
+
+    # x 좌표 이동 - 플레이어 자동차의 움직임 제어할 때 필요
+    def move_x(self):
+        self.rect.x += self.dx
+
+ 
+
+    # 화면 밖으로 못 나가게 방지
+    def check_screen(self):
+        if self.rect.right > WINDOW_WIDTH or self.rect.x < 0:
+            self.rect.x -= self.dx
+        if self.rect.bottom > WINDOW_HEIGHT or self.rect.y < 0:
+            self.rect.y -= self.dy
+
+ 
+
+    def jump(self, j):
+        self.isJump = j
+
+ 
+
     def update(self):
-        # 좌우 이동
-        if self.is_animating == True: 
-            if self.direction == True:    # 오른쪽
-               self.image = self.Rsprites[int(self.current_sprite)]
-               
-            else:       #왼쪽
-                self.image = self.sprites[int(self.current_sprite)]
-                self.velocity_x = abs(self.velocity_x ) * -1
-            
-            self.current_sprite += 0.2
-            
-            if self.current_sprite >= len(self.sprites):
-                self.current_sprite = 0
-                
-            
-            self.rect.x += self.velocity_x
+        # isJump 값이 0보다 큰지 확인
+        if self.isJump > 0:
+            # isJump 값이 2일 경우 속도를 리셋
+            # 점프 한 상태에서 다시 점프를 위한 값
 
-        # 공격
-        if self.is_attack == True:
-            self.velocity_x = 0
-            
-            if self.direction == True:
-                self.image = self.attack_Rsprites[int(self.current_attack_sprite)]
+            # 이 코드를 주석처리하면 이중점프를 못한다.
+            if self.isJump == 2:
+                self.v = VELOCITY
 
-            else :
-                self.image = self.attack_sprites[int(self.current_attack_sprite)]
-                
-            self.current_attack_sprite += 0.2
-                
-            if self.current_attack_sprite >= len(self.attack_sprites):
-                self.current_attack_sprite = 0
-                self.unanimate()
+ 
 
-# 스테이지
-stage = pygame.image.load("stage.png")
-stage_size = stage.get_rect().size
-stage_height = stage_size[1] #스테이지의 높이 위에 캐릭터를 두기 위해 사용
+            # 역학공식 계산 (F). F = 0.5 * mass * velocity^2.
+            if self.v > 0:
+                # 속도가 0보다 클때는 위로 올라감
+                F = (0.5 * self.m * (self.v * self.v))
+            else:
+                # 속도가 0보다 작을때는 아래로 내려감
+                F = -(0.5 * self.m * (self.v * self.v))
+
+ 
+
+            # 좌표 수정 : 위로 올라가기 위해서는 y 좌표를 줄여준다.
+            self.rect.y -= round(F)
+
+ 
+
+            # 속도 줄여줌
+            self.v -= 1
+
+ 
+
+            # 바닥에 닿았을때, 변수 리셋
+            if self.rect.bottom > WINDOW_HEIGHT:
+                self.rect.bottom = WINDOW_HEIGHT
+                self.isJump = 0
+                self.v = VELOCITY
+
+ 
+
+def main():
+    global SCREEN, WINDOW_WIDTH, WINDOW_HEIGHT
+
+ 
+
+    # pygame 초기화 및 스크린 생성
+    pygame.init()
+    SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    pygame.display.set_caption("Jump Test")
+
+ 
+
+    clock = pygame.time.Clock()
+
+ 
+
+    # 플레이어 자동차 생성
+    player = Car()
+    player.load_car()
+
+ 
+
+    playing = True
+
+    while playing:
+
+        # 키가 눌린 상태 체크
+        keys = pygame.key.get_pressed()
+        # 스페이스키가 눌려있고, isJump가 2라면 1로 변경한다.
+        # 이 작업을 해주지 않으면 스페이스가 눌려있는 상태면 플레이어가 계속 위로 올라가게 된다.
+        if (keys[pygame.K_SPACE]):
+            if player.isJump == 2:
+                player.jump(1)
 
 
-character = pygame.image.load("image/메인남캐_Move_01.gif")
-character_size = character.get_rect().size
-character_width = character_size[0]
-character_height = character_size[1]
-character_x_pos = (screen_width / 2) - (character_width / 2)
-character_y_pos = (screen_height - character_height - stage_height)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                playing = False
+                pygame.quit()
+                sys.exit()
 
-# 캐릭터 움직임
-moving_sprites = pygame.sprite.Group()
-player = Player(10,screen_height - stage_height - character_height)
-moving_sprites.add(player)
+ 
+
+            # 화살표 키를 이용해서 플레이어의 움직임 거리를 조정해준다.
+            # 키를 떼면 움직임 거리를 0으로 한다.
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    player.dx = 5
+                elif event.key == pygame.K_LEFT:
+                    player.dx = -5
 
 
-# 러닝
-running = True
-while running :
-    
-    screen.fill((0,0,0))
-    
-    # 2. 이벤트 처리 ( 키보드, 마우스 등 )
-    for event in pygame.event.get(): # 어떤 이벤트가 발생하는지
-        if event.type == pygame.QUIT: # 창이 닫히는 이벤트가 발생하면 ( 안쓰면 꺼지지 않음 )
-            running = False
-            
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                player.direction = True
-                player.animate()
-                
-            if event.key == pygame.K_LEFT:
-                player.direction = False
-                player.animate()
-            if event.key == pygame.K_SPACE:
-                player.is_attack = True
-                player.animate()
-            
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
-                player.unanimate()
+                # 스페이스키를 눌렀을 때,
+                # 0이면 바닥인 상태 : 1로 변경
+                # 1이면 점프를 한 상태 : 2로 변경, 점프한 위치에서 다시 점프를 하게 된다. 즉, 이중점프
+                if event.key == pygame.K_SPACE:
+                    if player.isJump == 0:
+                        player.jump(1)
+                    elif player.isJump == 1:
+                        player.jump(2)
 
-    if player.rect.x < 0:
-        player.rect.x = 0
-        
-    elif player.rect.x > screen_width - character_width:
-        player.rect.x = screen_width - character_width
-    
-    
-    
-    screen.blit(stage, (0, screen_height - stage_height))
-    
-    moving_sprites.draw(screen)
-    moving_sprites.update()
-    
-    
-    pygame.display.update() #게임 화면을 다시 그리기 ! (반드시 계속 호출 되어야 되는 부분)
-    
-    clock.tick(60) #게임 화면의 초당 프레임 수를 설정
-    
-# 파이게임 종료
-pygame.quit()
+ 
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    player.dx = 0
+                elif event.key == pygame.K_LEFT:
+                    player.dx = 0
+
+ 
+
+        # 배경색을 흰색으로
+        SCREEN.fill(WHITE)
+
+ 
+
+        ''' 게임 코드 작성 '''
+
+ 
+
+        # 플레이어를 스크린에 표시 및 화면 밖으로 못 벗어나게 하기
+        player.draw_car()
+        player.move_x()
+
+        # 플레이어의 y 좌표를 움직여주는 메서드 추가, 점프하는 것
+
+        player.update()
+        player.check_screen()
+
+ 
+
+        ''' 게임 코드 끝 '''
+
+ 
+
+        pygame.display.flip()
+
+ 
+
+        # 초당 프레임 설정
+        clock.tick(60)
+
+
+if __name__ == '__main__':
+    main()
